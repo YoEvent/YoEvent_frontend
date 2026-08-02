a5c2f6af5ce4952d90c6b8c7b95851200b3e17f4
Ngoh1919 <aghet87@gmail.com>
2026-08-02 18:36:11 +0100

    removed all useless .js files at the root of the project

--- check_inject.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('app/admin/events/page.tsx', 'utf8').split('\n');
-
-for (let i = 1135; i <= 1150; i++) {
-  console.log(`Line ${i}:`, lines[i]);
-}
--- find_btn.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('app/admin/events/page.tsx', 'utf8').split('\n');
-
-for (let i = 1220; i <= 1245; i++) {
-  console.log(`Line ${i}:`, lines[i]);
-}
